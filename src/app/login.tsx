808efe9b6abf8326ff7a9f02d1140519096ce00d
Fatima Dayanna Villanueva Flores <82072554+Dayanna452@users.noreply.github.com>
2024-07-18 12:18:54 -0500

    pages

--- src/app/login.tsx
+++ src/app/login.tsx
@@ -2,8 +2,6 @@ import Image from 'next/image'
 
 export default function Login () {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      El login
-    </main>
+    <Login />
   )
 }
