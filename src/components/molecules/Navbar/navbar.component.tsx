96d06a9f0c1511a43180555849fb0829ac3c8885
Fatima Dayanna Villanueva Flores <82072554+Dayanna452@users.noreply.github.com>
2024-07-18 13:56:54 -0500

    update navbar bg v3

--- src/components/molecules/Navbar/navbar.component.tsx
+++ src/components/molecules/Navbar/navbar.component.tsx
@@ -1,18 +1,14 @@
 import Image from 'next/image'
 import Link from 'next/link'
 
-const classesNavbar = {
-  nav: 'relative flex w-full flex-wrap items-center justify-between bg-zinc-50 py-2 shadow-dark-mild lg:py-4'
-}
-
 export const Navbar = () => {
   return (
-    <nav className='relative flex w-full flex-wrap items-center justify-between bg-neutral-400 py-2 shadow-dark-mild lg:py-4'>
-      <div className='flex w-full flex-wrap items-center justify-between px-3'>
+    <nav className='relative flex w-full flex-wrap items-center justify-center bg-neutral-400 py-2 shadow-dark-mild lg:py-4'>
+      <div className='flex w-full flex-wrap items-center justify-center px-3'>
         <div>
           <Link
             href='/'
-            className='mx-2 my-1 flex justify-center items-center lg:mb-0 lg:mt-0'
+            className='mx-2 my-1 flex items-center lg:mb-0 lg:mt-0'
           >
             <Image
               src='/home-icon.svg'
