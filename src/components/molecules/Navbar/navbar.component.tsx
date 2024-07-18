4d20368b409261b79bdc352b169e1d9d7e96ad45
Fatima Dayanna Villanueva Flores <82072554+Dayanna452@users.noreply.github.com>
2024-07-18 12:56:06 -0500

    update navbar

--- src/components/molecules/Navbar/navbar.component.tsx
+++ src/components/molecules/Navbar/navbar.component.tsx
@@ -1,5 +1,9 @@
 import Image from 'next/image'
 
+const classesNavbar = {
+  nav: 'relative flex w-full flex-wrap items-center justify-between bg-zinc-50 py-2 shadow-dark-mild dark:bg-neutral-700 lg:py-4',
+}
+
 export const Navbar = () => {
   return (
     <nav className='relative flex w-full flex-wrap items-center justify-between bg-zinc-50 py-2 shadow-dark-mild dark:bg-neutral-700 lg:py-4'>
@@ -7,9 +11,9 @@ export const Navbar = () => {
         <div>
           <a className='mx-2 my-1 flex items-center lg:mb-0 lg:mt-0' href='#'>
             <Image
-              src='../public/logo.png'
-              width={500}
-              height={500}
+              src='/vercel.svg'
+              width={100}
+              height={40}
               alt='Picture of the author'
             />
           </a>
