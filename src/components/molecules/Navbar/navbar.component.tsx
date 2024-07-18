fe17fdd822cd4fadf0759e74a0b58483d9bd38d9
Fatima Dayanna Villanueva Flores <82072554+Dayanna452@users.noreply.github.com>
2024-07-18 12:42:51 -0500

    navbar component

--- src/components/molecules/Navbar/navbar.component.tsx
+++ src/components/molecules/Navbar/navbar.component.tsx
@@ -1,9 +1,20 @@
-import { Button } from "app/components/atoms/Button"
+import Image from 'next/image'
 
 export const Navbar = () => {
   return (
-    <div>
-      <Button variant='primary'>Home</Button>
-    </div>
+    <nav className='relative flex w-full flex-wrap items-center justify-between bg-zinc-50 py-2 shadow-dark-mild dark:bg-neutral-700 lg:py-4'>
+      <div className='flex w-full flex-wrap items-center justify-between px-3'>
+        <div>
+          <a className='mx-2 my-1 flex items-center lg:mb-0 lg:mt-0' href='#'>
+            <Image
+              src='../public/logo.png'
+              width={500}
+              height={500}
+              alt='Picture of the author'
+            />
+          </a>
+        </div>
+      </div>
+    </nav>
   )
 }
