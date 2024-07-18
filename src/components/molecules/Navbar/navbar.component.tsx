647cdb4adc3948ff9b8705795ba9c26d9944960e
Fatima Dayanna Villanueva Flores <82072554+Dayanna452@users.noreply.github.com>
2024-07-18 13:02:26 -0500

    update navbar logo size

--- src/components/molecules/Navbar/navbar.component.tsx
+++ src/components/molecules/Navbar/navbar.component.tsx
@@ -1,7 +1,8 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 const classesNavbar = {
-  nav: 'relative flex w-full flex-wrap items-center justify-between bg-zinc-50 py-2 shadow-dark-mild dark:bg-neutral-700 lg:py-4',
+  nav: 'relative flex w-full flex-wrap items-center justify-between bg-zinc-50 py-2 shadow-dark-mild dark:bg-neutral-700 lg:py-4'
 }
 
 export const Navbar = () => {
@@ -9,14 +10,17 @@ export const Navbar = () => {
     <nav className='relative flex w-full flex-wrap items-center justify-between bg-zinc-50 py-2 shadow-dark-mild dark:bg-neutral-700 lg:py-4'>
       <div className='flex w-full flex-wrap items-center justify-between px-3'>
         <div>
-          <a className='mx-2 my-1 flex items-center lg:mb-0 lg:mt-0' href='#'>
+          <Link
+            href='/'
+            className='mx-2 my-1 flex items-center lg:mb-0 lg:mt-0'
+          >
             <Image
               src='/home-icon.svg'
-              width={100}
+              width={40}
               height={40}
               alt='Picture of the author'
             />
-          </a>
+          </Link>
         </div>
       </div>
     </nav>
