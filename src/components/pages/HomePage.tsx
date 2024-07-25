204d5ba80270d575f5b64ebfeecee2674407bf2f
Fatima Dayanna Villanueva Flores <82072554+Dayanna452@users.noreply.github.com>
2024-07-25 13:33:24 -0500

    users list

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -16,6 +16,13 @@ export const HomePage = () => {
   const data = [
     { id: '1', name: 'danna', email: 'danna@gmail.com', role: 'admin' }
   ]
+  console.log('users', allUsers?.users)
+
+  const newAllUsers = allUsers?.users.map((user: any) => {
+    const { id, name, email, role } = user
+    return { id, name, email, role }
+  })
+
   return (
     <Wrapper>
       <div className='flex w-full justify-end py-6'>
@@ -25,7 +32,7 @@ export const HomePage = () => {
       </div>
       <Table
         columns={['id', 'name', 'email', 'role', 'options']}
-        rows={data}
+        rows={newAllUsers ||[]}
         deleteButton={handleDeleteUser}
         updateButton={handleUpdateUser}
       />
